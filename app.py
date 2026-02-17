75ed03572cccaea90ff6ab32c0c5e87b11069aa5
NamanSundriyal011 <namansundriyal9@gmail.com>
2026-02-17 10:07:59 +0530

    Trigger CI

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Dockerized Construction Service Running"
+    return "Dockerized Construction Service Running v2"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
